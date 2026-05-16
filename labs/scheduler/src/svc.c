af95099f2ace0b9bbef1fde3ecfe9a0ada7bc90f
doruk-d <dorukanakat@gmail.com>
2026-05-16 21:25:36 +0200

    feat(scheduler): unify preemptive and cooperative scheduling with a common scheduler path

--- labs/scheduler/src/svc.c
+++ labs/scheduler/src/svc.c
@@ -1,8 +1,6 @@
 #include "scheduler.h"
 #include <stdint.h>
 
-#define SCB_ICSR (*(volatile uint32_t *)0xE000ED04)
-
 void SVC_Handler_c(uint32_t *frame){
     uint8_t svc_type = ((uint8_t *)frame[6])[-2]; // in non fault cases pc points to the next instruction
 
@@ -11,7 +9,7 @@ void SVC_Handler_c(uint32_t *frame){
             task_launch();
             break;
         case SYS_YIELD:
-            SCB_ICSR |= (1 << 28);
+            scheduler_run();
             break;
     }
 }
